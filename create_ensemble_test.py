fe23a9d5a3d4870f37962622d70d4e4fa7107f26
Sirofjelly <sillikuebler@gmail.com>
2024-05-06 15:24:05 +0200

    new create ensemble method

--- create_ensemble_test.py
+++ create_ensemble_test.py
@@ -1,11 +1,12 @@
-import numpy as np
 from itertools import product
 from random import choice
 import random
 from operator import itemgetter
+import numpy as np
 import itertools
+from scipy.spatial.distance import pdist, squareform
 from openset_imagenet.util import get_sets_for_ensemble, get_binary_output_for_class_per_model
-
+"""
 def hamming_distance_min_among_all(matrix, row=True):
     if row == True:
         # Calculate the Hamming distance between rows in the matrix and for every row get the minimum distance to other rows
@@ -34,13 +35,31 @@ def hamming_distance_min_among_all(matrix, row=True):
                     distances.append(distance)
             hamming_distances.append(min(distances))
         return min(hamming_distances)
-        
+"""
+
+def hamming_distance_min_among_all(matrix, row=True):
+    if row:
+        # Compute pairwise Hamming distances between rows
+        # pdist returns proportion, so we multiply by the number of columns
+        hamming_dist_matrix = squareform(pdist(matrix, 'hamming')) * matrix.shape[1]
+    else:
+        # Transpose the matrix for column-wise comparisons
+        # pdist returns proportion, so we multiply by the number of rows
+        hamming_dist_matrix = squareform(pdist(matrix.T, 'hamming')) * matrix.shape[0]
+
+    # set the diagonal to infinity since we don't want to consider distance of a row/column to itself
+    np.fill_diagonal(hamming_dist_matrix, np.inf)
+
+    # Find the minimum non-zero distance for each row/column
+    min_distances = np.min(hamming_dist_matrix, axis=1)
+
+    # Return the overall minimum
+    return np.min(min_distances)
 
 def create_matrix(number_of_models, number_of_classes):
     # Create a matrix containing all possible combinations of 0 and 1
     combinations = list(product([0, 1], repeat=number_of_classes))
     combinations = np.array(combinations)
-    print("Combinations: \n", combinations)
     print("Number of Combinations: ", len(combinations))  # should be 2^number_of_classes
 
     # remove vectors that begin with 1 to avoid inversion
@@ -87,23 +106,23 @@ def create_matrix(number_of_models, number_of_classes):
         matrix = np.vstack((matrix, balanced_combinations[random_max_sum_index]))
         # remove the selected vector from the list of balanced combinations
         balanced_combinations.pop(random_max_sum_index)
+        print("Matrix shape: ", matrix.shape)
     
     print("Matrix: \n", matrix)
     print("Matrix shape: ", matrix.shape)
-    print("row wise min hamming distance: ", hamming_distance_min_among_all(matrix, row=True))
-    print("column wise min hamming distance: ", hamming_distance_min_among_all(matrix, row=False))
+    print("Row wise min hamming distance - new algo: ", hamming_distance_min_among_all(matrix, row=True))
+    print("Column wise min hamming distance - new algo: ", hamming_distance_min_among_all(matrix, row=False))
     return matrix
 
-create_matrix(100, 10)
+create_matrix(number_of_models=30, number_of_classes=10)
 
-"""
+
 # Old method returns no matrices directly thats why we have to do some extra steps before calculating hamming distance
 
-number_of_models = 6
-classes = [0, 1, 2, 3]
+number_of_models = 30
+classes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 class_splits = get_sets_for_ensemble(classes, number_of_models)
 class_binary = get_binary_output_for_class_per_model(class_splits)
-print("class binaries: ", class_binary)
 
 # Convert the dictionary to a list of tuples
 class_binary_tuples = list(class_binary.items())
@@ -114,13 +133,5 @@ class_binary_tuples.sort(key=lambda x: x[0])
 class_binary_array = np.array([value for _, value in class_binary_tuples]).T
 
 
-hamming_distances = []
-total_hamming_distance = 0
-for i in range(len(classes)):
-    for j in range(i + 1, len(classes)):
-        total_hamming_distance += np.sum(class_binary_array[:, i] != class_binary_array[:, j])
-hamming_distances.append(total_hamming_distance)
-
-# Return the matrices and their corresponding Hamming distances
-print("Hamming distance old approach: ", set(hamming_distances))
-"""
+print("Row wise min hamming distance - old algo: ", hamming_distance_min_among_all(class_binary_array, row=True))
+print("Column wise min hamming distance - old algo: ", hamming_distance_min_among_all(class_binary_array, row=False))
