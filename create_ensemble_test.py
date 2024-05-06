f5ef65ded2c5d771f5e85a29f3a5045015670fde
Sirofjelly <sillikuebler@gmail.com>
2024-05-06 14:01:22 +0200

    create_matrix now updated

--- create_ensemble_test.py
+++ create_ensemble_test.py
@@ -2,15 +2,46 @@ import numpy as np
 from itertools import product
 from random import choice
 import random
+from operator import itemgetter
 import itertools
 from openset_imagenet.util import get_sets_for_ensemble, get_binary_output_for_class_per_model
+
+def hamming_distance_min_among_all(matrix, row=True):
+    if row == True:
+        # Calculate the Hamming distance between rows in the matrix and for every row get the minimum distance to other rows
+        hamming_distances = []
+        for i in range(matrix.shape[0]):
+            row = matrix[i]
+            distances = []
+            for j in range(matrix.shape[0]):
+                if i != j:
+                    # Calculate the Hamming distance between two rows
+                    distance = np.sum(row != matrix[j])
+                    distances.append(distance)
+            # Get the minimum distance for the current row
+            hamming_distances.append(min(distances))
+        # Return the minimum distance among all rows
+        return min(hamming_distances)
+    else:
+        # Calculate the Hamming distance between columns in the matrix and for every column get the minimum distance to other columns
+        hamming_distances = []
+        for i in range(matrix.shape[1]):
+            column = matrix[:, i]
+            distances = []
+            for j in range(matrix.shape[1]):
+                if i != j:
+                    distance = np.sum(column != matrix[:, j])
+                    distances.append(distance)
+            hamming_distances.append(min(distances))
+        return min(hamming_distances)
+        
 
 def create_matrix(number_of_models, number_of_classes):
     # Create a matrix containing all possible combinations of 0 and 1
     combinations = list(product([0, 1], repeat=number_of_classes))
     combinations = np.array(combinations)
-    print("Combinations: ", combinations)
-    print(len(combinations))  # should be 2^number_of_classes
+    print("Combinations: \n", combinations)
+    print("Number of Combinations: ", len(combinations))  # should be 2^number_of_classes
 
     # remove vectors that begin with 1 to avoid inversion
     combinations = [c for c in combinations if c[0] == 0]
@@ -24,30 +55,46 @@ def create_matrix(number_of_models, number_of_classes):
             balanced = np.logical_or(np.sum(combination) == number_of_classes // 2, np.sum(combination) == (number_of_classes + 1) // 2)
         if np.all(balanced):
             balanced_combinations.append(combination)
-    print("Balanced Combinations: ", balanced_combinations, "Length: ", len(balanced_combinations))
+    print("Balanced Combinations: ", balanced_combinations, "Length (Max possible Models): ", len(balanced_combinations))
 
     if len(balanced_combinations) < number_of_models:
         raise ValueError("Number of balanced combinations is less than number of models")
 
-    # Select unique vectors from balanced_combinations to maximize Hamming distance
-    selected_vectors = []
-    remaining_vectors = balanced_combinations.copy()
+    # randomly select the first vector to start the matrix
+    first_vector_index = random.randint(0, len(balanced_combinations) - 1)
+    matrix = balanced_combinations[first_vector_index]
+    # remove the start vector from the list of balanced combinations
+    balanced_combinations.pop(first_vector_index)
 
-"""
-    # construct all possible matrices with dim=0 equal to number_of_models$
-    matrices = 
+    # calculate the optimal next vector to add to the matrix
+    for _ in range(number_of_models - 1):
+        minimum_row_wise_distance = []
+        minimum_column_wise_distance = []
+        for balanced_combination in balanced_combinations:
+            matrix = np.vstack((matrix, balanced_combination))
+            minimum_row_wise_distance.append(hamming_distance_min_among_all(matrix, row=True))
+            minimum_column_wise_distance.append(hamming_distance_min_among_all(matrix, row=False))
+            # remove the last row from the matrix
+            matrix = matrix[:-1]
+        # get the index of the minimum distance
+        row_dist_and_column_dist = list(zip(minimum_row_wise_distance, minimum_column_wise_distance))
+        # find the tuple with the maximum sum
+        max_sum_tuple = max(row_dist_and_column_dist, key=lambda x: x[0] + x[1])
 
-    # Calculate the Hamming distance between selected vectors
-    hamming_distances = []
-    for i in range(number_of_models):
-        for j in range(i + 1, number_of_models):
-            hamming_distance = np.sum(matrix[:, i] != matrix[:, j])
-            hamming_distances.append(hamming_distance)
+        # get all possibles index of the maximum sum tuple
+        max_sum_index = [i for i, j in enumerate(row_dist_and_column_dist) if j == max_sum_tuple]
+        random_max_sum_index = choice(max_sum_index)
+        matrix = np.vstack((matrix, balanced_combinations[random_max_sum_index]))
+        # remove the selected vector from the list of balanced combinations
+        balanced_combinations.pop(random_max_sum_index)
+    
+    print("Matrix: \n", matrix)
+    print("Matrix shape: ", matrix.shape)
+    print("row wise min hamming distance: ", hamming_distance_min_among_all(matrix, row=True))
+    print("column wise min hamming distance: ", hamming_distance_min_among_all(matrix, row=False))
+    return matrix
 
-    print("Hamming distance: ", set(hamming_distances))
-    return matrix, hamming_distances
-"""
-create_matrix(6, 4)
+create_matrix(100, 10)
 
 """
 # Old method returns no matrices directly thats why we have to do some extra steps before calculating hamming distance
@@ -76,75 +123,4 @@ hamming_distances.append(total_hamming_distance)
 
 # Return the matrices and their corresponding Hamming distances
 print("Hamming distance old approach: ", set(hamming_distances))
-"""
-"""
-def create_matrix(number_of_classes, number_of_models):
-    # Create a matrix containing all possible combinations of 0 and 1
-    combinations = list(product([0, 1], repeat=number_of_classes))
-    combinations = np.array(combinations)
-    print("Combinations: ", combinations)
-    print(len(combinations))
-
-    # Generate all possible matrices based on the randomly selected first vector
-    matrices = []
-    first_vector = choice(combinations)
-    print("First Vector: ", first_vector)
-    remaining_combinations = [c for c in combinations if not np.array_equal(c, first_vector)]
-
-    for remaining_vectors in itertools.combinations(remaining_combinations, number_of_classes - 1):
-        remaining_vectors = np.array(remaining_vectors)
-        matrix = np.column_stack((first_vector, remaining_vectors.T))
-        matrices.append(matrix)
-    print(len(matrices))
-
-    # Filter out matrices with unbalanced 0 and 1 per row
-    balanced_matrices = []
-    for matrix in matrices:
-        row_sums = matrix.sum(axis=1)
-        if (number_of_classes % 2 == 0):
-            balanced = row_sums == number_of_classes // 2
-        else:
-            balanced = np.logical_or(row_sums == number_of_classes // 2, row_sums == (number_of_classes + 1) // 2)
-        if np.all(balanced):
-            balanced_matrices.append(matrix)
-    print("Balanced Matrixes: ", len(balanced_matrices))
-
-    # Filter out matrices with inverted rows and duplicate rows
-    unique_matrices = []
-    for matrix in balanced_matrices:
-        matrix_list = list(matrix)
-        inversed_rows_exist = False
-
-        # check if a row is duplicated in matrix
-        unk, count = np.unique(matrix, axis=0, return_counts=True)
-        if any(count > 1):
-            continue
-        
-        for row in matrix:
-            inverted = np.ones(len(row), dtype=np.int8) - row
-
-            if any(np.array_equal(inverted, m) for m in matrix_list):
-                inversed_rows_exist = True
-                break
-            
-        if not inversed_rows_exist:
-            unique_matrices.append(matrix)
-    print("No inversed rows and no duplicate rows: ", len(unique_matrices))
-    print(unique_matrices)
-
-    # Calculate the total Hamming distance between columns for each matrix
-    hamming_distances = []
-    for matrix in unique_matrices:
-        total_hamming_distance = 0
-        for i in range(number_of_classes):
-            for j in range(i + 1, number_of_classes):
-                total_hamming_distance += np.sum(matrix[:, i] != matrix[:, j])
-        hamming_distances.append(total_hamming_distance)
-
-    # Return the matrices and their corresponding Hamming distances
-    print("Hamming distance new approach: ", set(hamming_distances))
-    return unique_matrices, hamming_distances
-
-# New method
-create_matrix(3, 4)
 """
